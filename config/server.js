4d163a93abd2cb26cbab6357065ca7ce354059d7
ShenLong <buithanhlong091295@gmail.com>
2021-09-08 16:19:23 +0700

    first init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1a3a98db2f64c4dabee00e015081623c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07b972f4-a099-4771-9a03-d51b9f4e7643'
+};
