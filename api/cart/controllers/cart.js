f60b4550e9f66b4df9840326a827f157c3cee22a
Shen Long <shenlong@MacBook-Pro-cua-Shen.local>
2021-10-12 23:45:57 +0700

    get cart of me

--- api/cart/controllers/cart.js
+++ api/cart/controllers/cart.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async getCart(ctx) {
+        const query = {
+          userID: ctx.request.query["userID"],
+        };
+        const cart = await strapi
+          .query("carts")
+          .model.aggregate([{ $match: query }, { $sort: { createdAt: -1 } }, {$limit: 1}]);
+        if (cart && cart.products.length > 0) {
+            for (let i = 0; i < cart.products.length; i++) {
+                const product = await strapi.services.products.findOne({
+                    id: cart.products[i].productID,
+                });
+                if (product) {
+                    cart.products[i] = {
+                        productID: cart.products[i].productID,
+                        quantity: cart.products[i].quantity,
+                        amount: cart.products[i].amount,
+                        productName: product.name,
+                        productImage: product.image,
+                    }
+                }
+            }
+        }
+        return cart
+      }
+};
