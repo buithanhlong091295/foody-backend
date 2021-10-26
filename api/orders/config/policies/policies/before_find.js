643b2448afbb2d7557e09c84b9941c71d49fb2d7
Shen Long <shenlong@MacBook-Pro-cua-Shen.local>
2021-10-26 11:52:37 +0700

    orders

---
+++ api/orders/config/policies/policies/before_find.js
@@ -0,0 +1,18 @@
+"use strict";
+const _ = require('lodash');
+
+/**
+ * `phone-number-unique-post` policy.
+ */
+
+module.exports = async (ctx, next) => {
+    if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+          const { id } = await strapi.plugins[
+            'users-permissions'
+          ].services.jwt.getToken(ctx);
+    
+        ctx.request.query["userID"] = id
+        ctx.request.query["_sort"] = "createdAt:DESC"
+    }
+    await next();
+};
