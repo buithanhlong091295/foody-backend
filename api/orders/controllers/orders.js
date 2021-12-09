e59aa869c923694e9e2ee6465f7aba5261f3b8bc
Shen Long <shenlong@MacBook-Pro-cua-Shen.local>
2021-12-09 23:56:29 +0700

    statistics total orders by range time

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -90,5 +90,83 @@ module.exports = {
         }
         const res = await strapi.services.orders.find({ email: {$regex: ctx.request.query["email"]} }); 
         return res
-      }
+    },
+
+    async statisticsNumOrders(ctx) {
+        let from = new Date(parseInt(ctx.request.query["from"]))
+        let fromInt64 = parseInt(ctx.request.query["from"])
+        let to = new Date(parseInt(ctx.request.query["to"]))
+        let toInt64 = parseInt(ctx.request.query["to"])
+        const null_date = new Date(0);
+        const statistics = await strapi.query("orders").model.aggregate([
+            {
+                $match: {
+                  status: "SUCCESS",
+                  isPaid: true,
+                  createdAt: {
+                      $gte: from,
+                      $lte: to,
+                  }
+                },
+            },
+            {
+                $group: {
+                    _id: {
+                        "time": {
+                            "year":  {"$year": "$createdAt"},
+                            "month": {"$month": "$createdAt"},
+                            "day":   {"$dayOfMonth": "$createdAt"},
+                        },
+                    },
+                    totalOrders: {
+                        "$sum": 1,
+                    },
+                },
+            },
+            {
+                $addFields: {
+                    "timestamp": {
+                        "$divide": [
+                            {
+                                "$subtract": [
+                                {
+                                    "$dateFromParts": {
+                                    "year":  "$_id.time.year",
+                                    "month": "$_id.time.month",
+                                    "day":   "$_id.time.day",
+                                    }
+                                }, null_date],
+                            },
+                            1
+                        ]
+                    }
+                },
+            },
+            {
+                $sort: {
+                    "timestamp": 1
+                }
+            }
+        ])
+        // console.log(statistics)
+        let data = {}
+        for (let i = 0; i < statistics.length ; i++) {
+            data[statistics[i].timestamp] = statistics[i]
+        }
+        // console.log(data)
+        let res = []
+
+        for(let i = 0;fromInt64 <= toInt64; i++) {
+            let d = {
+                totalOrders: 0,
+                day: new Date(fromInt64)
+            }
+            if (data[fromInt64]) {
+                d["totalOrders"] = data[fromInt64].totalOrders
+            }
+            res.push(d)
+            fromInt64 += 86400000
+        }
+        return res
+    },
 };
