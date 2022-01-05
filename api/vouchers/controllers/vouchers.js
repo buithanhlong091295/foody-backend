7c743f2e7d16e8f56cccb0eb013b344fbeb757b7
Shen Long <shenlong@MacBook-Pro-cua-Shen.local>
2022-01-06 00:31:36 +0700

    sendmail

--- api/vouchers/controllers/vouchers.js
+++ api/vouchers/controllers/vouchers.js
@@ -5,4 +5,17 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async notifyByMail(ctx) {
+        const users = await strapi.plugins['users-permissions'].services.user.fetchAll()
+        // console.log(users)
+        for(let i = 0; i< users.length; i++) {
+          await strapi.plugins['email'].services.email.send({
+            to: users[i].email,
+            subject: "A VOUCHER FOR YOU",
+            text: "We have a voucher for you, enter " + ctx.request.body["code"] + " to get " + ctx.request.body["promotionAmount"] + "VND off for orders overs " + ctx.request.body["applyForOrderValue"] + "VDN, so open the Foody app and order now!"
+          })
+        }
+        return {status: "ok"}
+      }
+};
