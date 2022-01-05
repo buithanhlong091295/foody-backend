fa719e08e366dade99a48b1617547ffb32b7d6ad
Shen Long <shenlong@MacBook-Pro-cua-Shen.local>
2022-01-05 23:12:04 +0700

    sendmail

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -20,5 +20,18 @@ module.exports = {
     }
     const res = await strapi.services.product.find({ name: {$regex: ctx.request.query["name"]}, isDeleted: false }); 
     return res
+  },
+
+  async notifyByMail(ctx) {
+    const users = await strapi.plugins['users-permissions'].services.user.fetchAll()
+    // console.log(users)
+    for(let i = 0; i< users.length; i++) {
+      await strapi.plugins['email'].services.email.send({
+        to: users[i].email,
+        subject: "A NEW FOOD",
+        text: "We have a new food, it is " + ctx.request.body["productName"] + ", if you want to try it open the Foody app now!"
+      })
+    }
+    return {status: "ok"}
   }
 };
